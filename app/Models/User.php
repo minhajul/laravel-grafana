0bab7837a7fb0b076018b34040048634587d9b5a
Md Minhajul Islam <kmsohelinfo@gmail.com>
2026-03-14 21:21:10 +0600

    Updated

--- app/Models/User.php
+++ app/Models/User.php
@@ -4,11 +4,9 @@ declare(strict_types=1);
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Illuminate\Support\Facades\Hash;
 
 final class User extends Authenticatable
 {
@@ -27,12 +25,6 @@ final class User extends Authenticatable
 
     protected $casts = [
         'email_verified_at' => 'datetime',
+        'password' => 'hashed',
     ];
-
-    public function password(): Attribute
-    {
-        return new Attribute(
-            set: fn ($value) => Hash::make($value),
-        );
-    }
 }
